b00ce1ce460d3ec1e3230a24d9d0effe05af7033
erosswog <erosswog@gmail.com>
2016-08-06 23:25:52 -0400

    added addScore and updateHandicapIndex methods to Golfer.java

--- app/src/main/java/ersoftware/com/droidcaddy/Golfer.java
+++ app/src/main/java/ersoftware/com/droidcaddy/Golfer.java
@@ -1,5 +1,6 @@
 package ersoftware.com.droidcaddy;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -17,6 +18,79 @@ public class Golfer {
         history = new LinkedList<Score>();
     }
 
+    // Public Methods
+    public void addScore(Score newScore) {
+        history.addFirst(newScore);
+        updateHandicapIndex();
+    }
+
+    // Private Methods
+    private void updateHandicapIndex() {
+        int numDifferentialsUsed = 0;
+
+        switch(history.size()) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                numDifferentialsUsed = 1;
+                break;
+            case 7:
+            case 8:
+                numDifferentialsUsed = 2;
+                break;
+            case 9:
+            case 10:
+                numDifferentialsUsed = 3;
+                break;
+            case 11:
+            case 12:
+                numDifferentialsUsed = 4;
+                break;
+            case 13:
+            case 14:
+                numDifferentialsUsed = 5;
+                break;
+            case 15:
+            case 16:
+                numDifferentialsUsed = 6;
+                break;
+            case 17:
+                numDifferentialsUsed = 7;
+                break;
+            case 18:
+                numDifferentialsUsed = 8;
+                break;
+            case 19:
+                numDifferentialsUsed = 9;
+                break;
+            case 20:
+                numDifferentialsUsed = 10;
+                break;
+            default:
+                if(history.size() > 20) {
+                    numDifferentialsUsed = 10;
+                }
+                else {
+                    numDifferentialsUsed = 0;
+                }
+                break;
+        }
+
+        float sumDifferentials = 0.0f;
+        Iterator<Score> iter = history.iterator();
+        int count = 0;
+
+        while(count < numDifferentialsUsed) {
+            Score currentScore = iter.next();
+            sumDifferentials += currentScore.getDifferential();
+            count++;
+        }
+        handicapIndex = 0.96f * (sumDifferentials / (float)numDifferentialsUsed);
+    }
+
     // Getters and Setters
     public String getName() {
         return name;
@@ -26,3 +100,4 @@ public class Golfer {
         return handicapIndex;
     }
 }
+
